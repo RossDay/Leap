e75748b16117b822cedbf2034b2eb4175035aaa6
Ross Day <rossday@gmail.com>
2013-08-05 17:23:24 -0500

    Simpler access to some Hand properties through extension methods

---
+++ LeapSandboxWPF/LeapExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Leap;
+
+namespace LeapSandboxWPF
+{
+	public static class LeapExtensions
+	{
+		/*
+				// Get the hand's normal vector and direction
+				Vector normal = hand.PalmNormal;
+				Vector direction = hand.Direction;
+
+
+ 				SafeWriteLine("Hand pitch: " + direction.Pitch * 180.0f / (float)Math.PI + " degrees, "
+	              + "roll: " + normal.Roll * 180.0f / (float)Math.PI + " degrees, "
+	              + "yaw: " + direction.Yaw * 180.0f / (float)Math.PI + " degrees");
+*/
+		public static float RollDegress(this Hand hand)
+		{
+			return hand.PalmNormal.Roll*180.0f/(float) Math.PI;
+		}
+		public static float PitchDegress(this Hand hand)
+		{
+			return hand.Direction.Pitch * 180.0f / (float)Math.PI;
+		}
+		public static float YawDegress(this Hand hand)
+		{
+			return hand.Direction.Yaw * 180.0f / (float)Math.PI;
+		}
+	}
+}
