59d5f011ca6c9e0f5896366c0aca7025d3f81489
Ross Day <rossday@gmail.com>
2013-08-28 02:21:02 -0500

    Mode switching...prevent if new mode set would just reverse the switch.

--- LeapSandboxWPF/ActionDispatcher.cs
+++ LeapSandboxWPF/ActionDispatcher.cs
@@ -18,7 +18,7 @@ namespace Vyrolan.VMCS
         private Dictionary<BaseTrigger, BaseAction> _ActiveMap;
 
         #region UpdateActiveMap
-        private void UpdateActiveMap()
+        private bool UpdateActiveMap(string modeName, bool isActivate)
         {
             string actionName;
             BaseAction action;
@@ -31,58 +31,125 @@ namespace Vyrolan.VMCS
                     actionName = mode.GetActionForTrigger(triggerPair.Key);
                     if (String.IsNullOrEmpty(actionName))
                         continue; // trigger not in mode
-                    if (!_Actions.TryGetValue(actionName, out action))
+                    if (actionName.StartsWith("AM:") || actionName.StartsWith("DM:"))
+                        action = new ModeAction(actionName); // TODO - make a factory and intern these
+                    else if (!_Actions.TryGetValue(actionName, out action))
                         continue; // must be bad config
                     map.Add(triggerPair.Value, action);
                     break;
                 }
 
+            // check that new map won't instantly deactivate/reactivate
+            if (
+                map.Where(p =>
+                    p.Key.IsTriggered
+                    && !(p.Key is GestureTrigger)
+                    && p.Value is ModeAction
+                    && ((ModeAction)p.Value).IsMatch(modeName, !isActivate)
+                ).Count() > 0
+            )
+                return false;
+
             // check all triggered Triggers in current map to see if action is gone/different in new map
-            foreach (var mapPair in _ActiveMap.Where(p => p.Key.IsTriggered))
+            foreach (var mapPair in _ActiveMap.Where(p => p.Key.IsTriggered && !(p.Key is GestureTrigger)))
                 if (!map.TryGetValue(mapPair.Key, out action))
                     // mapping is gone...action should end
-                    action.End();
+                    EndAction(action);
                 else if (!action.Equals(mapPair.Value))
                 {
                     // different action...end old, begin new
-                    action.End();
-                    mapPair.Value.Begin();
+                    EndAction(action);
+                    BeginAction(mapPair.Value);
                 }
 
             // begin any new triggered action not present in current
             foreach (var mapPair in map)
-                if (!_ActiveMap.ContainsKey(mapPair.Key))
-                    // mapping is gone...action should end
-                    mapPair.Value.Begin();
+                if (mapPair.Key.IsTriggered && !(mapPair.Key is GestureTrigger) && !_ActiveMap.ContainsKey(mapPair.Key))
+                    BeginAction(mapPair.Value);
 
             _ActiveMap = map;
+            return true;
         } 
         #endregion
 
         #region Activate/Deactivate Mode
-        public void ActivateMode(string modeName)
+        private void ActivateMode(string modeName)
         {
             lock (_Lock)
             {
                 ConfigurationMode mode;
                 if (!_Modes.TryGetValue(modeName, out mode))
                     return; // must  be bad config
-                _ActiveModes.Remove(mode);
+
+                if (_ActiveModes.First.Value.Equals(mode))
+                    return;
+
+                var temp = _ActiveModes.Find(mode);
+                if (temp != null)
+                {
+                    var holder = temp.Previous;
+                    _ActiveModes.Remove(temp);
+                    temp = holder;
+                }
+
                 _ActiveModes.AddFirst(mode);
-                UpdateActiveMap();
+
+                if (!UpdateActiveMap(modeName, true))
+                {
+                    _ActiveModes.RemoveFirst();
+                    if (temp != null)
+                        _ActiveModes.AddAfter(temp, mode);
+                }
             }
         }
-        public void DeactivateMode(string modeName)
+        private void DeactivateMode(string modeName)
         {
             lock (_Lock)
             {
                 ConfigurationMode mode;
                 if (!_Modes.TryGetValue(modeName, out mode))
                     return; // must  be bad config
-                _ActiveModes.Remove(mode);
-                UpdateActiveMap();
+
+                var temp = _ActiveModes.Find(mode);
+                if (temp == null)
+                    return;
+
+                var prev = temp.Previous;
+                _ActiveModes.Remove(temp);
+                if (!UpdateActiveMap(modeName, false))
+                {
+                    if (prev == null)
+                        _ActiveModes.AddFirst(mode);
+                    else
+                        _ActiveModes.AddAfter(prev, mode);
+                }
             }
         }
+        #endregion
+
+        #region BeginAction / EndAction
+        private void BeginAction(BaseAction action)
+        {
+            var modeAction = action as ModeAction;
+            if (modeAction != null)
+            {
+                if (modeAction.IsActivate)
+                    ActivateMode(modeAction.ModeName);
+                else
+                    DeactivateMode(modeAction.ModeName);
+            }
+            else
+                action.Begin();
+        }
+
+        private void EndAction(BaseAction action)
+        {
+            var modeAction = action as ModeAction;
+            if (modeAction != null && modeAction.IsActivate)
+                DeactivateMode(modeAction.ModeName);
+            else
+                action.End();
+        } 
         #endregion
 
         #region OnTriggerChanged
@@ -93,9 +160,9 @@ namespace Vyrolan.VMCS
                 if (!_ActiveMap.TryGetValue((BaseTrigger)sender, out action))
                     return;
             if (e.IsTriggered)
-                action.Begin();
+                BeginAction(action);
             else
-                action.End();
+                EndAction(action);
         }
         #endregion
 
--- LeapSandboxWPF/Actions/ModeAction.cs
+++ LeapSandboxWPF/Actions/ModeAction.cs
@@ -7,9 +7,19 @@ namespace Vyrolan.VMCS.Actions
 {
     class ModeAction : BaseAction
     {
+        public string ModeName { get; private set; }
+        public bool IsActivate { get; private set; }
+
         public ModeAction(string name)
             : base(name)
         {
+            ModeName = name.Substring(2);
+            IsActivate = name.StartsWith("AM:");
+        }
+
+        public bool IsMatch(string modeName, bool isActivate)
+        {
+            return ModeName.Equals(modeName) && IsActivate.Equals(isActivate);
         }
 
         protected override void BeginImpl()
